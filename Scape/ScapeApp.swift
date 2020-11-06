c8a40c408f7dade81c4a916bb5a588983f9a9101
0xLeif <zmeriksen@icloud.com>
2020-11-05 18:05:36 -0600

    Initial Commit

---
+++ Scape/ScapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScapeApp.swift
+//  Scape
+//
+//  Created by Zach Eriksen on 11/5/20.
+//
+
+import SwiftUI
+
+@main
+struct ScapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
